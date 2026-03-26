3da17f8484b60ce2d73ef89f13885ad745f6ccf6
healer <xushnudbekvaxobov0421@gmail.com>
2026-03-26 22:42:52 +0500

    mmm

--- src/main/java/imebel/imebel/exception/GlobalExceptionHandler.java
+++ src/main/java/imebel/imebel/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package imebel.imebel.exception;
 import imebel.imebel.dto.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailSendException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,6 +23,40 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ApiResponse<>(false, "Validation error", errors, 400));
+    }
+    @ExceptionHandler(AppBadException.class)
+    public ResponseEntity<ApiResponse<?>> handleAppBadException(AppBadException ex){
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ApiResponse<>(false, "Bad request", errors, 400));
+    }
+
+    @ExceptionHandler(DataNotFoundException.class)
+    public ResponseEntity<ApiResponse<?>> handleDataNotFoundException(DataNotFoundException ex){
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ApiResponse<>(false, "Data not found", errors, 404));
+    }
+
+    @ExceptionHandler(MailSendException.class)
+    public ResponseEntity<?> handleMailException(MailSendException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new  ApiResponse<>(false, "Internal server error", ex.getMessage(), 500));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiResponse<?>> handleRuntimeException(RuntimeException ex){
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ApiResponse<>(false, "Internal server error", errors, 500));
     }
 
 }
